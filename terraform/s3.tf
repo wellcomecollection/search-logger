daa10f1d8bd1e57fe266a7032a7d8701341ec8db
James A Gorrie <j.gorrie@wellcome.ac.uk>
2019-04-02 15:55:23 +0100

    UTF-8 over ascii

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -2,6 +2,10 @@ resource "aws_s3_bucket" "search-logger" {
   bucket = "search-logger"
   acl    = "private"
 
+  versioning {
+    enabled = true
+  }
+
   tags = {
     Service = "${local.service_name}"
   }
